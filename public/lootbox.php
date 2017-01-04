3bbbbf7d776e651364fc955d5f6c0f2ab3797091
Tititesouris <tititesouris@laposte.net>
2017-01-04 14:22:20 +0100

    Removed direct access to Session and Get from templates

--- public/lootbox.php
+++ public/lootbox.php
@@ -1,7 +1,13 @@
 <?php
 require_once("required.php");
 if (isUserLoggedIn()) {
-    echo $twig->render("lootbox.twig");
+    echo $twig->render("lootbox.twig", [
+        "USER" => $_SESSION["user"],
+        "HEROES" => $_SESSION["heroes"],
+        "TYPES" => $_SESSION["types"],
+        "COSMETICS" => $_SESSION["cosmetics"],
+        "USERSETTINGS" => $_SESSION["user"]->getSettings()
+    ]);
 }
 else {
     header("Location: ./login.php");
